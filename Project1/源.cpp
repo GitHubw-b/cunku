a46093e9fac48265fe64c6174a78c080ad10400d
GitHubw-b <934434451@qq.com>
2019-11-04 23:01:38 +0800

    151

--- Project1/源.cpp
+++ Project1/源.cpp
@@ -20,48 +20,54 @@ int main(int argc, char* argv[])
 	cin >> a[3];
 	cout << "请输入是否选择分数(有->1;   无->0):" << endl;
 	cin >> a[4];
+	cout << "请输入几列：" << endl;
+	cin >> a[5];
 	for (int i = 1; i <= a[0]; i++)      //由用户决定打印多少道题a[0]
 	{
-		if (a[4] == 0) {
-			k1 = rand() % (a[2]);   //随机产生a[2]以内的数,a[2]是由用户输入的数值范围
-			k2 = rand() % (a[2]);
-			if (a[3] == 0) {
-				k1 = rand() % (a[2]);   //随机产生a[2]以内的数,a[2]是由用户输入的数值范围
-				k2 = rand() % (a[2]);
-				if (a[3] == 0)         //控制结果没有负数，如果相减出现负数，则交换两个数
+		if (a[4] == 0) 
+		{
+		
+		 k1 = rand() % (a[2]);   //随机产生a[2]以内的数,a[2]是由用户输入的数值范围
+		 k2 = rand() % (a[2]);
+		    if (a[3] == 0)         //控制结果没有负数，如果相减出现负数，则交换两个数
+			{
+				if (k1 < k2)
 				{
-					if (k1 < k2)
-					{
 						temp = k1;
 						k1 = k2;
 						k2 = temp;
-					}
 				}
-				else
-					k1 = rand() % (a[2]);
-				k2 = rand() % (a[2]);
-				if (a[1] == 1) {           //a[1]为控制乘除参数
+			}
+		else
+			k1 = rand() % (a[2]);
+			k2 = rand() % (a[2]);
+			if (a[1] == 1) //a[1]为控制乘除参数
+			{           
 					ch = rand() % 4;      //ch有四种情况，加减乘除
+			}
+			else
+				ch = rand() % 2;      //ch有两种情况，加减，没有乘除
+				if ((i + 1) % a[5] == 0)      //a[5]为打印列数，式子个数除以列数的余数决定了它的位置
+				{
+					cout << endl;
 				}
-				else
-					ch = rand() % 2;      //ch有两种情况，加减，没有乘除
 
 				switch (ch)
 				{
-				case 0:
+				 case 0:
 					cout << k1 << "+" << k2 << "=" << "\t\t";
 					break;
-				case 1:
+				 case 1:
 					cout << k1 << "-" << k2 << "=" << "\t\t";
 					break;
-				case 2:
+				 case 2:
 					cout << k1 << "*" << k2 << "=" << "\t\t";
 					break;
-				case 3:
+				 case 3:
 					cout << k1 << "/" << k2 << "=" << "\t\t";
 					break;
-				}
-			}
+		        }
+		}
 			else
 			{
 				int n1, n2, n3, n4;
@@ -94,9 +100,8 @@ int main(int argc, char* argv[])
 					cout << "(" << n1 << "/" << n2 << ")/(" << n3 << "/" << n4 << ")=" << "\t\t";
 					break;
 				}
-			}
-		}
+		    }
+	}
 		cout << endl;
 		return 0;
-	}
 }
